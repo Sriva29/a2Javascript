1bdf128d4fd312c55ab2160cdbd8da10c9956b9d
Sriva29 <63369374+Sriva29@users.noreply.github.com>
2023-10-16 12:43:48 -0400

    All requirements completed

--- js/main.js
+++ js/main.js
@@ -1,27 +1,100 @@
-// visitor data
-let visitorData = [
-    {month:"January", visitors: 23},
-    {month:"February", visitors: 27},
-    {month:"March", visitors: 38},
-    {month:"April", visitors: 33},
-    {month: "May", visitors: 100},
-    {month: "June", visitors: 25}
-]
+// GDPR cookies
+const cookiesSection = document.querySelector(".cookies");
+const cookiesAcceptButton = document.querySelector(".acceptCookies");
+const cookiesRevokeButton = document.createElement("a");
+cookiesRevokeButton.textContent = "Revoke Cookies";
+cookiesRevokeButton.href = "#";
+const cookiesText = document.querySelector(".cookiesText"); //p tag from cookies text
 
-//console.log(visitorData.length);
+cookiesAcceptButton.addEventListener("click", acceptHandler);
 
-const visitorTable = document.querySelector(".visitor-table");
+function acceptHandler(){
+    console.log("Cookies Accepted!");
+    cookiesText.textContent = "Cookies were accepted. Would you like to revoke? ";
+    cookiesText.appendChild(cookiesRevokeButton);
+}
 
-console.log(visitorTable);
+cookiesRevokeButton.addEventListener("click", revokeHandler);
 
-for (let i=0; i<visitorData.length; i++){
+function revokeHandler(){
+    console.log("Cookies Revoked 😡");
+    cookiesText.textContent = "This site uses cookies. They are all mandatory because who wouldn't want moustache-related cookies in their system. 😊";
+    cookiesText.appendChild(cookiesAcceptButton);
+}
+
+let highContBool = false;
+
+//button contrast switch
+
+document.querySelector("#contrastButton").addEventListener("click", contrastToggle);
+
+document.addEventListener("keydown", (e)=>{
+    console.log(e.key);
+    console.log(e.ctrlKey);
     
-    const tableRow = document.createElement("tr");
-    const tdMonth = document.createElement("td");
-    tdMonth.textContent = visitorData[i].month;
-    const tdVisitors = document.createElement("td");
-    tdVisitors.textContent = visitorData[i].visitors;
-    tableRow.appendChild(tdMonth);
-    tableRow.appendChild(tdVisitors);
-    visitorTable.appendChild(tableRow);
+    if(e.key == "a" && e.ctrlKey)
+    {
+        e.preventDefault();
+    contrastToggle();
+    }
+    });
+
+function contrastToggle(){
+    if(!highContBool)
+    {
+        console.log("Change DOM to High Contrast 👁‍🗨");
+        highContrastMode();
+    } else {
+        console.log("Regular contrast pour toi 🥐");
+        regularContrastMode();
+    }
+}
+
+let defaultColor = document.body.style.color;
+let defaultBgColor = document.body.style.backgroundColor;
+let defaultPadding = document.body.style.padding;
+let defaultLineHeight = document.body.lineHeight;
+let defaultLetterSpacing = document.body.letterSpacing;
+let logo = document.querySelector("#logo");
+let logoArt = document.querySelector("#logoArt");
+let allAs = document.querySelectorAll("a");
+
+// High contrast
+function highContrastMode(){
+    // color
+    document.body.style.color = "greenyellow";
+    document.body.style.backgroundColor = "black";
+    //spacing
+    document.body.style.padding = "10px";
+    document.body.style.lineHeight = "1.5";
+    document.body.style.letterSpacing = "2px";
+
+    
+    allAs.forEach(link =>{
+        link.style.color = "#00f0a0";
+    })
+    //if(logo.hasAttribute("src")) logo.setAttribute("src", "img/moustache-inverted.png");
+    if(logo !=null) logo.setAttribute("src", "img/moustache-inverted.png");
+    //if(logoArt.hasAttribute("src")) logoArt.setAttribute("src", "../img/moustache-inverted.png");
+    if(logoArt !=null) logoArt.setAttribute("src", "../img/moustache-inverted.png");
+    highContBool = true;
+}
+
+//regular contrast
+function regularContrastMode(){
+    document.body.style.color = defaultColor;
+    document.body.style.backgroundColor = defaultBgColor;
+
+    document.body.style.padding = defaultPadding;
+    document.body.style.lineHeight = defaultLineHeight;
+    document.body.style.letterSpacing = defaultLetterSpacing;
+
+    allAs.forEach(link =>{
+        link.style.color = "blue";
+    })
+
+    if(logo != null) logo.setAttribute("src", "img/moustache.png");
+    if(logoArt !=null) logoArt.setAttribute("src", "../img/moustache.png");
+
+    highContBool = false;
 }
---
+++ js/visitor.js
@@ -0,0 +1,28 @@
+// visitor data
+let visitorData = [
+    {month:"January", visitors: 23},
+    {month:"February", visitors: 27},
+    {month:"March", visitors: 38},
+    {month:"April", visitors: 33},
+    {month: "May", visitors: 100},
+    {month: "June", visitors: 25}
+]
+
+//console.log(visitorData.length);
+
+const visitorTable = document.querySelector(".visitor-table");
+
+console.log(visitorTable);
+
+for (let i=0; i<visitorData.length; i++){
+    
+    const tableRow = document.createElement("tr");
+    const tdMonth = document.createElement("td");
+    tdMonth.textContent = visitorData[i].month;
+    const tdVisitors = document.createElement("td");
+    tdVisitors.textContent = visitorData[i].visitors;
+    tableRow.appendChild(tdMonth);
+    tableRow.appendChild(tdVisitors);
+    visitorTable.appendChild(tableRow);
+}
+
